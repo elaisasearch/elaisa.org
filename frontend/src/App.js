2c8639b14e8c34e4a459cfc11c5e2e760aed59b9
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-26 17:30:10 +0100

    added NavigationBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavigationBar from './components/NavigiationBar/NavigationBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavigationBar/>
       </div>
     );
   }
