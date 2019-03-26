d2e5ce002fb8f777f09cf3809047b03a9e47bf33
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-26 17:30:44 +0100

    NavigationBar styles

---
+++ frontend/src/assets/jss/NavigationBarStyle.js
@@ -0,0 +1,13 @@
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    appBar: {
+      backgroundColor: "transparent"
+    },
+    menu: {
+        color: "black"
+    }
+  };
+
+export default styles;
