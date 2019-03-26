dd0bf3e2b70e0a3825a59bc219e5f2aeb7172807
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-26 18:42:14 +0100

    added seachbar, img, dialogs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 import NavigationBar from './components/NavigiationBar/NavigationBar';
+import SearchBar from './components/SearchBar/SearchBar';
+
+//logo
+import logo from './assets/img/logo.png';
+
+import LangLevelDialog from './components/LangLevelDialog/LangLevelDialog';
+import ResultsLanguageDialog from './components/ResultsLanguageDialog/ResultsLanguageDialog';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
-        <NavigationBar/>
+        <NavigationBar />
+        <img src={logo} style={{ width: "20%", marginTop: "5%" }} alt="Pizza"></img>
+        <SearchBar />
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <LangLevelDialog />
+          <ResultsLanguageDialog />
+        </div>
       </div>
     );
   }
