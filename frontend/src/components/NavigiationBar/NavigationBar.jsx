161a44c2955e5de8354b1c01f0aca8129f273f95
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-27 08:54:09 +0100

    added left menu component

--- frontend/src/components/NavigiationBar/NavigationBar.jsx
+++ frontend/src/components/NavigiationBar/NavigationBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 //import styles
 import styles from '../../assets/jss/NavigationBarStyle';
+import LeftMenu from '../Menu/LeftMenu';
 
 const NavigationBar = (props) => {
 
@@ -13,9 +12,9 @@ const NavigationBar = (props) => {
     <div style={styles.root}>
       <AppBar position="static" id="navBar" style={styles.appBar}>
         <Toolbar>
-          <IconButton style={styles.menu} aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          <div>
+            <LeftMenu/>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
