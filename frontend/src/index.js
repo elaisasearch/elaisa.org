aec8b7b02e27478019d369899ac598d039840307
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-27 10:53:03 +0100

    added global routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from "history";
+import { Router, Switch, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// views
+import App from './App';
+import Results from './views/Results';
+
+// Build the browser history
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={hist}>
+        <Switch>
+            <Route path='/results' component={Results} />
+            <Route path='/' component={App} />
+        </Switch>
+    </Router>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
