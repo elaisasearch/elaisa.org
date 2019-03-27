0c72c8356ef464897837b242c60bae9faa5b5935
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-27 10:04:06 +0100

    search bar for results view

---
+++ frontend/src/components/SearchBar/SearchBarNavigationBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+// styles 
+import styles from '../../assets/jss/SearchBarNavigationBarStyle';
+
+
+const SearchBar = (props) => {
+    const { classes } = props;
+
+    return (
+        <div style={styles.root}>
+            <TextField
+                id="outlined-full-width"
+                style={{ margin: 8, borderColor: "grey" }}
+                placeholder="Search for documents"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                InputProps={{
+                    classes: {
+                      root: classes.cssOutlinedInput,
+                      focused: classes.cssFocused,
+                      notchedOutline: classes.notchedOutline,
+                    },
+                  }}
+            />
+        </div>
+    );
+}
+
+export default withStyles(styles)(SearchBar);
