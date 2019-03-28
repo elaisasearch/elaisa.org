7181dab5bf818606e7ede03acd40a537924e8b63
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-28 13:29:01 +0100

    added lang/level pickers

--- frontend/src/components/NavigiationBar/NavigationBar.jsx
+++ frontend/src/components/NavigiationBar/NavigationBar.jsx
@@ -8,18 +8,24 @@ import styles from '../../assets/jss/NavigationBarStyle';
 import LeftMenu from '../Menu/LeftMenu';
 import SearchBarNavigationBar from '../SearchBar/SearchBarNavigationBar';
 
+import DropDownMenu from '../DropDownMenu/DropDownMenu';
+
 const NavigationBar = (props) => {
 
   const renderSearchBar = (props) => {
     const searchValue = props.value
     if (props.results) {
-      return <SearchBarNavigationBar value={searchValue} />
+      return <div style={{display: "flex", flexGrow: 1}}>
+        <SearchBarNavigationBar value={searchValue}/>
+        <DropDownMenu desc="Result Language" items={["Deutsch", "English", "Español"]} />
+        <DropDownMenu desc="Language Level" items={["A1", "A2", "B1", "B2", "C1", "C2"]} />
+      </div>
     }
   }
 
   const renderFilterBar = (props) => {
     if (props.results) {
-      return <div style={{marginLeft:"9vh", padding:"1vh"}}>
+      return <div style={{ marginLeft: "9vh", padding: "1vh" }}>
         <Button>All</Button>
         <Button>News</Button>
         <Button>Blogs</Button>
