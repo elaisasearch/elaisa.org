034780d1461586fecaa86d67d5e219ecb6403e8a
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-28 10:45:27 +0100

    filter buttons in results view

--- frontend/src/components/NavigiationBar/NavigationBar.jsx
+++ frontend/src/components/NavigiationBar/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button'
 
 //import styles
 import styles from '../../assets/jss/NavigationBarStyle';
@@ -12,7 +13,17 @@ const NavigationBar = (props) => {
   const renderSearchBar = (props) => {
     const searchValue = props.value
     if (props.results) {
-      return <SearchBarNavigationBar value={searchValue}/>
+      return <SearchBarNavigationBar value={searchValue} />
+    }
+  }
+
+  const renderFilterBar = (props) => {
+    if (props.results) {
+      return <div style={{marginLeft:"9vh", padding:"1vh"}}>
+        <Button>All</Button>
+        <Button>News</Button>
+        <Button>Blogs</Button>
+      </div>
     }
   }
 
@@ -22,10 +33,11 @@ const NavigationBar = (props) => {
       <AppBar position="static" id="navBar" style={styles.appBar}>
         <Toolbar>
           <div>
-            <LeftMenu/>
+            <LeftMenu />
           </div>
           {renderSearchBar(props)}
         </Toolbar>
+        {renderFilterBar(props)}
       </AppBar>
     </div>
   );
