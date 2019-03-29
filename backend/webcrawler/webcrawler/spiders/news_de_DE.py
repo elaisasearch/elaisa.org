2a8c393d3808711fbf76e15c8a847f069d7a83a0
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-29 17:45:05 +0100

    concats text to one string // only store german websites

--- backend/webcrawler/webcrawler/spiders/news_de_DE.py
+++ backend/webcrawler/webcrawler/spiders/news_de_DE.py
@@ -13,18 +13,22 @@ class NewsSpider(scrapy.Spider):
     def parse(self, response):
         url = response.url
         for data in response.css('html'):
-            yield {
-                'url': url,
-                'meta': {
-                    'keywords': data.css('meta[name*=eywords]::attr(content)').get(),
-                    'author': data.css("meta[name*=uthor]::attr(content)").get(),
-                    'publisher': data.css("meta[name*=ublisher]::attr(content)").get(),
-                    'desc': data.css("meta[name*=escription]::attr(content)").get(),
-                    'date': data.css("meta[name*=ate]::attr(content)").get()
-                },
-                'title': data.css('title::text').get(),
-                'abstract': data.css('strong::text').get(),
-                'text': data.css('p::text').getall()
-            }
+            if data.css('html::attr(lang)').get() == "de":
+                yield {
+                    'url': url,
+                    'meta': {
+                        'language': data.css('html::attr(lang)').get(),
+                        'keywords': data.css('meta[name*=eywords]::attr(content)').get(),
+                        'author': data.css("meta[name*=uthor]::attr(content)").get(),
+                        'publisher': data.css("meta[name*=ublisher]::attr(content)").get(),
+                        'desc': data.css("meta[name*=escription]::attr(content)").get(),
+                        'date': data.css("meta[name*=ate]::attr(content)").get()
+                    },
+                    'title': data.css('title::text').get(),
+                    'abstract': data.css('strong::text').get(),
+                    'text': "".join(str(element) for element in data.css('p::text').getall()) 
+                }
+            else: 
+                continue
         for a in response.css('a::attr(href)'):
             yield response.follow(a, callback=self.parse)
