854ea8f61cbd48dec6844f1de4ab1b3e31397192
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-31 17:19:09 +0200

    stores inverted index to mongo db

--- backend/inverted_index/indexer.py
+++ backend/inverted_index/indexer.py
@@ -1,10 +1,13 @@
 from pprint import pprint as pp
-try: reduce
-except: from functools import reduce
+try:
+    reduce
+except:
+    from functools import reduce
 from collections import Counter
 from pymongo import MongoClient
 
- # source: http://rosettacode.org/wiki/Inverted_index#Python
+# source: http://rosettacode.org/wiki/Inverted_index#Python
+
 
 def parseMongo():
     client = MongoClient('mongodb://localhost:27017/')
@@ -17,65 +20,78 @@ def parseMongo():
         texts[str(news["_id"])] = txt
     return texts, words
 
+
 texts, words = parseMongo()
-#print('\nTexts')
-#pp(texts)
-#print('\nWords')
-#pp(sorted(words))
+# print('\nTexts')
+# pp(texts)
+# print('\nWords')
+# pp(sorted(words))
 
 terms = ["sie", "wie"]
 
 
-def termsearch(terms): # Searches full inverted index
+def termsearch(terms):  # Searches full inverted index
     if not set(terms).issubset(words):
         return set()
     return reduce(set.intersection,
                   (set(x[0] for x in txtindx)
                    for term, txtindx in finvindex.items()
                    if term in terms),
-                  set(texts.keys()) )
- 
+                  set(texts.keys()))
+
+
 def phrasesearch(phrase):
     wordsinphrase = phrase.strip().strip('"').split()
     if not set(wordsinphrase).issubset(words):
         return set()
-    #firstword, *otherwords = wordsinphrase # Only Python 3
+    # firstword, *otherwords = wordsinphrase # Only Python 3
     firstword, otherwords = wordsinphrase[0], wordsinphrase[1:]
     found = []
     for txt in termsearch(wordsinphrase):
         # Possible text files
-        for firstindx in (indx for t,indx in finvindex[firstword]
+        for firstindx in (indx for t, indx in finvindex[firstword]
                           if t == txt):
             # Over all positions of the first word of the phrase in this txt
-            if all( (txt, firstindx+1 + otherindx) in finvindex[otherword]
-                    for otherindx, otherword in enumerate(otherwords) ):
+            if all((txt, firstindx+1 + otherindx) in finvindex[otherword]
+                    for otherindx, otherword in enumerate(otherwords)):
                 found.append(txt)
     return found
- 
- 
-finvindex = {word:set((txt, wrdindx)
-                      for txt, wrds in texts.items()
-                      for wrdindx in (i for i,w in enumerate(wrds) if word==w)
-                      if word in wrds)
+
+
+finvindex = {word: set((txt, wrdindx)
+                       for txt, wrds in texts.items()
+                       for wrdindx in (i for i, w in enumerate(wrds) if word == w)
+                       if word in wrds)
              for word in words}
- 
-#print('\nFull Inverted Index')
-pp({k:sorted(v) for k,v in finvindex.items()})
- 
+
+
+def storeIndexInMongo(finvindex):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client["LanguageLevelSearchEngine"]
+    collection = db["inverted_index_de_DE"]
+
+    for word, docs in finvindex.items():
+        collection.insert_one({"word": word, "documents": list(docs)})
+
+
+storeIndexInMongo(finvindex)
+
 print('\nTerm Search on full inverted index for: ' + repr(terms))
 pp(sorted(termsearch(terms)))
- 
+
 phrase = '"beim dritten Mal"'
 print('\nPhrase Search for: ' + phrase)
 print(phrasesearch(phrase))
- 
+
 # Show multiple match capability
 phrase = '"Die Polizei"'
 print('\nPhrase Search for: ' + phrase)
 ans = phrasesearch(phrase)
 print(ans)
 ans = Counter(ans)
-try: 
-    print('  The phrase is found most commonly in text: ' + repr(ans.most_common(1)[0][0]))
-except: 
-    print('  The phrase is found most commonly in text: ' + repr(ans.most_common(1)))
+try:
+    print('  The phrase is found most commonly in text: ' +
+          repr(ans.most_common(1)[0][0]))
+except:
+    print('  The phrase is found most commonly in text: ' +
+          repr(ans.most_common(1)))
