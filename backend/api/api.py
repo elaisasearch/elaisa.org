fc6ef9d0bf198397e6a8d2b55ad996ea044acfe9
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-01 21:15:06 +0200

    return found docs from mongo // basics

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,9 +1,26 @@
 from bottle import route, run
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
+from json import JSONEncoder
 
 # source: https://gotofritz.net/blog/weekly-challenge/restful-python-api-bottle/
 
-@route('/')
-def hello():
-    return "Hello World!"
+#JSON Encoder
+class MongoEncoder(JSONEncoder):
+    def default(self, obj, **kwargs):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        else:            
+            return JSONEncoder.default(obj, **kwargs)
+
+@route('/find&query=<term>', method="GET")
+def find(term):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client["LanguageLevelSearchEngine"]
+    objdb = db["inverted_index_de_DE"].find({'word': term}).skip(0).limit(3)
+    entries = [entry for entry in objdb]
+    return json.dumps(entries, cls=MongoEncoder)
+
 
 run(host='localhost', port=8080, debug=True)
