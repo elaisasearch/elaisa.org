246197a9b00dd745acdfcc132b240f6bf21db3b1
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-02 13:30:28 +0200

    added axios get request to api

--- frontend/src/views/Results.js
+++ frontend/src/views/Results.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import NavigationBar from '../components/NavigiationBar/NavigationBar';
+import ResultList from '../components/ResultList/ResultList';
+import axios from 'axios';
 
 class Results extends React.Component {
 
-    render() {
-        const searchValue = this.props.location.state.searchValue;
-        const language = this.props.location.state.language;
-        const level = this.props.location.state.level
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchValue: props.location.state.searchValue,
+            language: props.location.state.language,
+            level: props.location.state.level
+        }
+        this.getResultDocs();
+    }
 
+    getResultDocs = () => {
+        // source: https://github.com/axios/axios
+        // IMPORTANT: install this on chrome: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related
+        axios.get(`http://localhost:8080/find&query=${this.state.searchValue}`)
+            .then(function (response) {
+                // handle success
+                console.log(response.data[0].documents);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+    render() {
         return (
             <div>
-                <NavigationBar results values={[searchValue, language, level]}/>
+                <NavigationBar results values={[this.state.searchValue, this.state.language, this.state.level]} />
+                <ResultList/>
             </div>
         );
     }
