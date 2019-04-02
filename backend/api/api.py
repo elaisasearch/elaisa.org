1193fcbfd96b1b5dde553a4d5cf0f0a055dbfad7
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-02 12:45:11 +0200

    basic docs return for single query

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,10 +1,15 @@
 from bottle import route, run
 from pymongo import MongoClient
+import pymongo
 from bson.objectid import ObjectId
 import json
 from json import JSONEncoder
+try:
+    reduce
+except:
+    from functools import reduce
+from search import *
 
-# source: https://gotofritz.net/blog/weekly-challenge/restful-python-api-bottle/
 
 #JSON Encoder
 # https://stackoverflow.com/questions/28251835/from-pymongo-objectid-import-objectid-importerror-no-module-named-objectid
@@ -15,11 +20,14 @@ class MongoEncoder(JSONEncoder):
         else:            
             return JSONEncoder.default(obj, **kwargs)
 
-@route('/find&query=<term>', method="GET")
-def find(term):
+@route('/find&query=<query>', method="GET")
+def find(query):
+    terms = query.split()
+    #words = getWordsFromMongo()
+    #documents = termsearch(terms, words)
     client = MongoClient('mongodb://localhost:27017/')
     db = client["LanguageLevelSearchEngine"]
-    objdb = db["inverted_index_de_DE"].find({'word': term}).skip(0).limit(3)
+    objdb = db["inverted_index_de_DE"].find({'word': terms[0]}).skip(0).limit(3)
     entries = [entry for entry in objdb]
     return json.dumps(entries, cls=MongoEncoder)
 
