475103708083d1140c721b1db32c4a67a5f4c1bd
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-03 13:36:21 +0200

    added access control

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,4 +1,5 @@
-from bottle import route, run
+from bottle import route, run, response
+import bottle
 from pymongo import MongoClient
 import pymongo
 from bson.objectid import ObjectId
@@ -20,8 +21,26 @@ class MongoEncoder(JSONEncoder):
         else:            
             return JSONEncoder.default(obj, **kwargs)
 
-@route('/find&query=<query>', method="GET")
+# source: https://stackoverflow.com/questions/17262170/bottle-py-enabling-cors-for-jquery-ajax-requests
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
+@route('/find&query=<query>', method=["OPTIONS","GET"])
+@enable_cors
 def find(query):
+    response.headers['Content-type'] = 'application/json'
+
     client = MongoClient('mongodb://localhost:27017/')
     db = client["LanguageLevelSearchEngine"]
     col = db["news_de_DE"]
