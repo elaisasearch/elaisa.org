45c6841a0714c6170b3e4384bff91aa75382d3ec
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-03 13:42:50 +0200

    first try to implement progress circle

--- frontend/src/components/ResultList/ResultList.jsx
+++ frontend/src/components/ResultList/ResultList.jsx
@@ -3,43 +3,67 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ResultItem from './ResultItem';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: '70%',
-    display: 'flex',
-    flexDirection: 'column',
-    marginLeft: '5%',
-    marginTop: '2%'
-  },
+    root: {
+        width: '100%',
+        maxWidth: '70%',
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: '5%',
+        marginTop: '2%'
+    },
 });
 
-class ResultList extends React.Component{
+class ResultList extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-
     }
 
-    render(){
+    renderList = (resultDocs, classes) => {
+        return <List className={classes.root}>
+            {resultDocs.map(doc => (
+                <ResultItem website={doc.url} title={doc.title} desc={doc.meta.desc} keywords={doc.meta.keywords} date={doc.meta.date} language={doc.meta.language} />
+            ))}
+        </List>
+    }
+
+    renderProgress = (value) => {
+        return <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '70vh'
+        }}>
+            <CircularProgress value={value}/>
+        </div>
+    }
+
+    // renderItems = (resultDocs, classes) => {
+    //     let i = 0
+    //     while (i <= resultDocs.length) {
+    //         i = i + 1;
+    //         console.log(i)
+    //         return this.renderProgress()
+    //     }
+    //     return this.renderList(resultDocs, classes)
+    // }
+
+    render() {
         const { classes } = this.props;
 
         const { resultDocs } = this.props;
 
-        return (
-          <List className={classes.root}>
-            {resultDocs.map(doc => (
-                <ResultItem website={doc.url} title={doc.title} desc={doc.meta.desc} keywords={doc.meta.keywords} date={doc.meta.date} language={doc.meta.language}/>
-            ))}
-          </List>
-        );
+        return this.renderList(resultDocs, classes);
     }
 
 }
 
 ResultList.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(ResultList);
