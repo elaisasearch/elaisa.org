c226cd12bae864420d3538836c7621df46049bd4
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-13 17:47:24 +0200

    better cors

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,36 +1,29 @@
-from bottle import route, run, response
-import bottle
-
+from bottle import Bottle, request, response, run
 # import lib files
 from lib import search, wikipedia
 
+app = Bottle()
 
-# source: https://stackoverflow.com/questions/17262170/bottle-py-enabling-cors-for-jquery-ajax-requests
-# the decorator
-def enable_cors(fn):
-    def _enable_cors(*args, **kwargs):
-        # set CORS headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
-        if bottle.request.method != 'OPTIONS':
-            # actual request; reply with the actual response
-            return fn(*args, **kwargs)
-
-    return _enable_cors
-
-@route('/find&query=<query>', method=["OPTIONS","GET"])
-@enable_cors
+@app.route('/find&query=<query>', method=["OPTIONS","GET"])
 def find(query):
     response.headers['Content-type'] = 'application/json'
 
     return search.findDocuments(query)
 
-@route('/wiki&term=<term>&language=<language>', method=["OPTIONS","GET"])
-@enable_cors
+@app.route('/wiki&term=<term>&language=<language>', method=["OPTIONS","GET"])
 def getInformationFromWikipedia(term, language):
+    response.headers['Content-type'] = 'application/json'
+
     return wikipedia.getWikiEntry(term, language)
 
-
-run(host='localhost', port=8080, debug=True)
+app.run(host='localhost', port=8080, debug=True)
