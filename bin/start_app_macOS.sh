5cffa578ca41fcc08453614e8edd21285f8aab21
Alexander Teusz <teusz.alexander@gmail.com>
2019-04-16 15:42:23 +0200

    updated for new folder structure

--- bin/start_app_macOS.sh
+++ bin/start_app_macOS.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 osascript <<END 
 tell application "Terminal"
-    do script "cd \"`pwd`\" && pip3 install -r backend/requirements.txt;python backend/api/api.py"
+    do script "cd \"`pwd`\" && pip3 install -r ../requirements.txt;python ../services/service-api/api.py"
 end tell
 END
 
 osascript <<END 
 tell application "Terminal"
-    do script "cd \"`pwd`\" && cd frontend/;yarn && yarn start"
+    do script "cd \"`pwd`\" && cd ../services/service-ui/;yarn && yarn start"
 end tell
 END
 
