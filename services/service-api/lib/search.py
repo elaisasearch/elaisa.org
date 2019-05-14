bba3651405c214cce319d812e375df9d4494d39a
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-14 10:33:07 +0200

    added level filter

--- services/service-api/lib/search.py
+++ services/service-api/lib/search.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 from json import JSONEncoder
 
 # get database info
-# BUG: no such file or dictionary ERROR ... 
+# BUG: no such file or dictionary ERROR ...
 #f = open("../../../bin/globals.json", "r")
 
 GLOBALS = {
@@ -24,16 +24,19 @@ GLOBALS = {
     }
 }
 
-#JSON Encoder
+# JSON Encoder
 # https://stackoverflow.com/questions/28251835/from-pymongo-objectid-import-objectid-importerror-no-module-named-objectid
+
+
 class MongoEncoder(JSONEncoder):
     def default(self, obj, **kwargs):
         if isinstance(obj, ObjectId):
             return str(obj)
-        else:            
+        else:
             return JSONEncoder.default(obj, **kwargs)
 
-def findDocuments(query):
+
+def findDocuments(query, level, language):
     client = MongoClient(GLOBALS["mongo"]["client"])
     db = client[GLOBALS["mongo"]["database"]]
     col = db[GLOBALS["mongo"]["collections"]["crawled"]["news"][0]]
@@ -42,17 +45,24 @@ def findDocuments(query):
     docs = getIdsFromWord(terms)
     docIds = [id[0] for id in docs]
 
+    print(docIds)
+
     documents = []
     for id in docIds:
-        query= {"_id": ObjectId(id)}
-        documents.append(col.find(query)[0])
+        query = {"_id": ObjectId(id), "level": level}
+        results = col.find(query)
+
+        for r in results: 
+            documents.append(r)
 
     return json.dumps(documents, cls=MongoEncoder)
+
 
 def getIdsFromWord(terms):
     client = MongoClient(GLOBALS["mongo"]["client"])
     db = client[GLOBALS["mongo"]["database"]]
     # TODO: Allow multiple terms search
-    objdb = db[GLOBALS["mongo"]["collections"]["inverted_index"][0]].find({'word': terms[0]}).skip(0)
+    objdb = db[GLOBALS["mongo"]["collections"]
+               ["inverted_index"][0]].find({'word': terms[0]}).skip(0)
     entries = [entry for entry in objdb]
     return entries[0]["documents"]
