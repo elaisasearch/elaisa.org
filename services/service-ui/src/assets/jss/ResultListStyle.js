08a47ff782c981aeb59f5c92f452b4dd5874f036
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-20 18:56:39 +0200

    outsourced style

---
+++ services/service-ui/src/assets/jss/ResultListStyle.js
@@ -0,0 +1,24 @@
+const styles = {
+    root: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: '5%',
+        marginTop: '2%'
+    },
+    resultDocsLength: {
+        marginLeft: '18vh',
+        marginTop: '2vh'
+    },
+    resultListRoot: {
+        marginRight: '10vh'
+    },
+    progress: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70vh'
+    }
+}
+
+export default styles;
--- services/service-ui/src/components/ResultList/ResultList.jsx
+++ services/service-ui/src/components/ResultList/ResultList.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ResultItem from './ResultItem';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Typography } from '@material-ui/core';
+import styles from "../../assets/jss/ResultListStyle";
 
-const styles = theme => ({
-    root: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        marginLeft: '5%',
-        marginTop: '2%'
-    },
-    resultDocsLength: {
-        martginLeft: '10%',
-        marginTop: '2%'
-    }
-});
 
 class ResultList extends React.Component {
 
-    renderList = (resultDocs, classes) => {
+    renderList = (resultDocs) => {
 
-        return <div style={{'marginRight': '10vh'}}>
-            <Typography style={{marginLeft: '18vh', marginTop: '2vh'}} variant="caption">{`${this.props.resultDocsLength} results for "${this.props.searchValue}"`}</Typography>
-            <List className={classes.root}>
+        return <div style={styles.resultListRoot}>
+            <Typography style={styles.resultDocsLength} variant="caption">{`${this.props.resultDocsLength} results for "${this.props.searchValue}"`}</Typography>
+            <List className={styles.root}>
                 {resultDocs.map(doc => (
                 <ResultItem website={doc.url} title={doc.title} desc={doc.meta.desc} keywords={doc.meta.keywords} date={doc.meta.date} language={doc.meta.language} level={doc.level} />
                 ))}
@@ -35,12 +21,7 @@ class ResultList extends React.Component {
     }
 
     renderProgress = (value) => {
-        return <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '70vh'
-        }}>
+        return <div style={styles.progress}>
             <CircularProgress value={value}/>
         </div>
     }
@@ -56,15 +37,11 @@ class ResultList extends React.Component {
     // }
 
     render() {
-        const { classes, resultDocs } = this.props;
+        const { resultDocs } = this.props;
         
-        return this.renderList(resultDocs, classes);
+        return this.renderList(resultDocs);
     }
 
 }
 
-ResultList.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(ResultList);
+export default ResultList;
