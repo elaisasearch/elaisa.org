e535213c89386f14a3f586e12dbcc62817ca937c
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-20 19:09:11 +0200

    outsourced style for wikiCardTemplate

--- services/service-ui/src/components/WikiCard/WikiCardTemplate.jsx
+++ services/service-ui/src/components/WikiCard/WikiCardTemplate.jsx
@@ -1,42 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-const styles = {
-  card: {
-    minWidth: 275,
-    maxWidth: 275,
-    maxHeight: 280,
-    marginTop: '6vh',
-    marginRight: '10vh',
-    marginLeft: 'auto'
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-};
+import styles from '../../assets/jss/WikiCardTemplateStyle';
 
 const  WikiCardTemplate = (props) => {
-  const { classes } = props;
   const { url, title, summary } = props;
 
   return (
-    <Card className={classes.card}>
+    <Card style={styles.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
+        <Typography style={styles.title} color="textSecondary" gutterBottom>
           Wikipedia
         </Typography>
         <Typography variant="h5" component="h2">
@@ -54,8 +30,4 @@ const  WikiCardTemplate = (props) => {
   );
 }
 
-WikiCardTemplate.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(WikiCardTemplate);
+export default WikiCardTemplate;
