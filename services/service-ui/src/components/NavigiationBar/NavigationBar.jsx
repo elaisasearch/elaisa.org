f021ab8c6c016eb4b2274d42dcab3efa34c44712
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-21 19:41:54 +0200

    direct call instead of using variables

--- services/service-ui/src/components/NavigiationBar/NavigationBar.jsx
+++ services/service-ui/src/components/NavigiationBar/NavigationBar.jsx
@@ -50,13 +50,10 @@ class NavigationBar extends React.Component {
 
   renderSearchBar = (props) => {
     if (props.results) {
-      const searchValue = props.values[0]
-      const language = props.values[1]
-      const level = props.values[2]
       return <div style={styles.searchBar}>
-        <SearchBarNavigationBar value={searchValue} onChange={e => this.setState({ value: e })} onKeyDown={this.keyPress} />
-        <DropDownMenu value={language} desc="Result Language" items={["Deutsch", "English", "Español"]} values={["de", "en", "es"]} onChange={e => this.setState({ language: e })} />
-        <DropDownMenu value={level} desc="Language Level" items={["A1", "A2", "B1", "B2", "C1", "C2"]} values={["A1", "A2", "B1", "B2", "C1", "C2"]} onChange={e => this.setState({ level: e })} />
+        <SearchBarNavigationBar value={props.values[0]} onChange={e => this.setState({ value: e })} onKeyDown={this.keyPress} />
+        <DropDownMenu value={props.values[1]} desc="Result Language" items={["Deutsch", "English", "Español"]} values={["de", "en", "es"]} onChange={e => this.setState({ language: e })} />
+        <DropDownMenu value={props.values[2]} desc="Language Level" items={["A1", "A2", "B1", "B2", "C1", "C2"]} values={["A1", "A2", "B1", "B2", "C1", "C2"]} onChange={e => this.setState({ level: e })} />
       </div>
     }
   }
