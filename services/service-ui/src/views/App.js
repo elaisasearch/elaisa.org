f278245e7ac5f2c79a3d9cc616d92f5656582a00
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-24 17:19:35 +0200

    transformed jss to css for App.js

--- services/service-ui/src/views/App.js
+++ services/service-ui/src/views/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavigationBar from '../components/NavigiationBar/NavigationBar';
 import SearchBar from '../components/SearchBar/SearchBar';
 import Footer from '../components/Footer/Footer';
-import styles from '../assets/jss/AppStyle'
+import '../assets/css/AppStyle.css'
 
 //logo
 import logo from '../assets/img/logo.png';
@@ -11,10 +11,10 @@ class App extends Component {
 
   render() {
     return (
-      <div style={styles.root}>
-        <div style={styles.app}>
+      <div className="root">
+        <div className="app">
           <NavigationBar />
-          <img id="logo" src={logo} style={styles.logo} alt="Elaisa Search Engine Logo"></img>
+          <img id="logo" src={logo} className="logo" alt="Elaisa Search Engine Logo"></img>
           <SearchBar />
         </div>
         <Footer />
