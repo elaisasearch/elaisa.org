7d3935243ba17f383a1c3b661f2d826b10346f20
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-24 17:30:53 +0200

    transformed jss to css for Footer.jsx

--- services/service-ui/src/components/Footer/Footer.jsx
+++ services/service-ui/src/components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import styles from '../../assets/jss/FooterStyle.js'
+import '../../assets/css/FooterStyle.css'
 import { Typography } from "@material-ui/core";
 import FooterPerson from './FooterPerson';
 
 const Footer = () => (
-    <footer style={styles.footer}>
-        <div style={styles.content}>
+    <footer className="footer">
+        <div className="content">
             <FooterPerson name="Jennifer Gynp" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="jennifer.gynp@hhu.de" />
             <FooterPerson name="Alexander Teusz" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="alexander.teusz@hhu.de" />
             <FooterPerson name="Paula Leberer" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="paula.leberer@hhu.de" />
         </div>
-        <div style={styles.legal}>
+        <div className="legal">
             <Typography variant="caption" color="default">
                 Elaisa - Language Level Search Engine - All rights reserved - 2019
             </Typography>
