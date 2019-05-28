852024ffa80db697e3d3d4e728599683dbe0e397
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-28 14:04:29 +0200

    typo

--- services/service-api/api.py
+++ services/service-api/api.py
@@ -12,7 +12,7 @@ def enable_cors():
     """
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
-response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @app.route('/')
 def index():
