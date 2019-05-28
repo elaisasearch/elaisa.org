b36c4a09d193f077eed8656a0f06060f06f78cff
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-28 15:17:44 +0200

    bugfix find needs cors

--- services/service-api/api.py
+++ services/service-api/api.py
@@ -4,6 +4,16 @@ from lib import search, wikipedia, user
 
 app = Bottle()
 
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
 @app.route('/')
 def index():
     return "API - Language Level Search Engine"
