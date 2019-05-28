fcb4b05eee5099a80529d77001e74699193969d6
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-28 20:04:23 +0200

    added change password form

--- services/service-ui/src/views/Account.js
+++ services/service-ui/src/views/Account.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import NavigationBar from '../components/NavigiationBar/NavigationBar';
@@ -6,38 +6,84 @@ import '../assets/css/AccountStyle.css';
 import Avatar from '@material-ui/core/Avatar';
 import Gravatar from 'react-gravatar';
 import { Person } from '@material-ui/icons/';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const Account = (props) => {
 
-    console.log(props.location.state);
+class Account extends Component {
 
-    const { email, firstname, lastname } = props.location.state;
+    state = {
+        oldPass: "",
+        newPass: ""
+    }
 
-    const renderAvatar = (props) => {
+    renderAvatar = (email, firstname, lastname) => {
         if (email) {
-            return <Gravatar email={email} size={100}/>
+            return <Gravatar email={email} size={100} />
         } else if (firstname && lastname) {
             return `${firstname.slice(0, 1)}${lastname.slice(0, 1)}`
         }
         return <Person />
     }
 
-    return <div>
-        <NavigationBar />
-        <div className="accountView">
-            <Paper className="accountPaper">
-                <Avatar alt={firstname} id="accountLogo">
-                    {renderAvatar(props)}
-                </Avatar>
-                <Typography variant="h3" color="textSecondary" component="h3" id="accountName">
-                    {firstname} {lastname}
-                </Typography>
-                <Typography variant="h5" color="textSecondary" id="accountEmail">
-                    {email}
-                </Typography>
-            </Paper>
+    handleChangeButton = () => {
+        console.log(this.state);
+    }
+
+    render() {
+        const { email, firstname, lastname } = this.props.location.state;
+
+        return <div>
+            <NavigationBar />
+            <div className="accountView">
+                <Paper className="accountPaper">
+                    <Avatar alt={firstname} id="accountLogo">
+                        {this.renderAvatar(email, firstname, lastname)}
+                    </Avatar>
+                    <Typography variant="h3" color="textSecondary" component="h3" id="accountName">
+                        {firstname} {lastname}
+                    </Typography>
+                    <Typography variant="h5" color="textSecondary" id="accountEmail">
+                        {email}
+                    </Typography>
+                    <div className="passwordDiv">
+                        <Typography variant="h6" color="textSecondary" id="accountEmail">
+                            Change your Password
+                    </Typography>
+                        <div className="passwordTextfieldsDiv">
+                            <TextField
+                                onChange={e => this.setState({ oldPass: e.target.value })}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Old Password"
+                                type="password"
+                                id="oldPassword"
+                                autoComplete="current-password"
+                                style={{ margin: "3%" }}
+                            />
+                            <TextField
+                                onChange={e => this.setState({ newPass: e.target.value })}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="New Password"
+                                type="password"
+                                id="newPassword"
+                                style={{ margin: "3%" }}
+                            />
+                        </div>
+                        <Button variant="contained" onClick={this.handleChangeButton}>
+                            Change
+                    </Button>
+                    </div>
+
+                </Paper>
+            </div>
         </div>
-    </div>
+    }
 }
 
 export default Account;
