20fb5168a0bd36cd560188e209cc9c7f59e4b4dc
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-29 17:39:37 +0200

    added navBar // 5% marginTop for pieChart

--- services/service-ui/src/views/Profile.js
+++ services/service-ui/src/views/Profile.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import PieChart from '../components/Profile/charts/PieChart';
+import NavigationBar from '../components/NavigiationBar/NavigationBar';
 
 class Profile extends Component {
-	render() {
-		
-		return (
-		<div>
-            <PieChart dataPoints={
-                [
-                    { y: 32, label: "Health" },
-                    { y: 22, label: "Finance" },
-                    { y: 15, label: "Education" },
-                    { y: 19, label: "Career" },
-                    { y: 5, label: "Family" },
-                    { y: 7, label: "Real Estate" }
-                ]
-            } />
-		</div>
-		);
-	}
+    render() {
+
+        return (
+            <div>
+                <NavigationBar />
+                <div style={{marginTop: "5%"}}>
+                    <PieChart dataPoints={
+                        [
+                            { y: 32, label: "Health" },
+                            { y: 22, label: "Finance" },
+                            { y: 15, label: "Education" },
+                            { y: 19, label: "Career" },
+                            { y: 5, label: "Family" },
+                            { y: 7, label: "Real Estate" }
+                        ]
+                    } />
+                </div>
+            </div>
+        );
+    }
 }
 export default Profile; 
