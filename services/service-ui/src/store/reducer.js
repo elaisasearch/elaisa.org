3dc4d79f343983c92ca32db2cfeeb27815dbb6f3
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-30 15:58:57 +0200

    added redux intialState to Account view

--- services/service-ui/src/store/reducer.js
+++ services/service-ui/src/store/reducer.js
@@ -1,9 +1,12 @@
 const initialState = {
-    loggedIn: false
+    loggedIn: false,
+    email: "frodo.beutlin@hobbits.com",
+    firstname: "Frodo",
+    lastname: "Beutlin"
 }
 
 const reducer = (state = initialState, action) => {
-
+    return state;
 }
 
 export default reducer;
