51bfdf6e14e084962a1ecd19ed91b5fbbff458ad
Alexander Teusz <teusz.alexander@gmail.com>
2019-05-31 11:32:08 +0200

    added redux to Profile view

--- services/service-ui/src/views/Profile.js
+++ services/service-ui/src/views/Profile.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PieChart from '../components/Profile/charts/PieChart';
 import NavigationBar from '../components/NavigiationBar/NavigationBar';
+import { connect } from 'react-redux';
 
 class Profile extends Component {
     render() {
 
+        // redux state
+        const { loggedIn, email, firstname, lastname } = this.props;
+
         return (
             <div>
-                <NavigationBar />
+                <NavigationBar loggedIn={loggedIn} email={email} firstname={firstname} lastname={lastname}/>
                 <div style={{marginTop: "5%"}}>
                     <PieChart dataPoints={
                         [
@@ -24,4 +28,14 @@ class Profile extends Component {
         );
     }
 }
-export default Profile; 
+
+const mapStateToProps = state => {
+    return {
+        loggedIn: state.loggedIn,
+        email: state.email,
+        firstname: state.firstname,
+        lastname: state.lastname
+    };
+};
+
+export default connect(mapStateToProps)(Profile); 
