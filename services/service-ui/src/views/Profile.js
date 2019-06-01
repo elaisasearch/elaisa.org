4bca49f58ee850de3591a6fe433970ea2fcf38d6
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-01 11:09:16 +0200

    initial card style for stats

--- services/service-ui/src/views/Profile.js
+++ services/service-ui/src/views/Profile.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import PieChart from '../components/Profile/charts/PieChart';
 import NavigationBar from '../components/NavigiationBar/NavigationBar';
-import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { Paper } from '@material-ui/core';
+import '../assets/css/ProfileStyle.css';
 
 class Profile extends Component {
     render() {
 
         // redux state
-        const { loggedIn, email, firstname, lastname } = this.props;
+        const { loggedIn, email, firstname, lastname } = this.props;
 
         return (
             <div>
-                <NavigationBar loggedIn={loggedIn} email={email} firstname={firstname} lastname={lastname}/>
-                <div style={{marginTop: "5%"}}>
-                    <PieChart dataPoints={
-                        [
-                            { y: 7, label: "A1" },
-                            { y: 5, label: "A2" },
-                            { y: 19, label: "B1" },
-                            { y: 65, label: "B2" },
-                            { y: 3, label: "C1" },
-                            { y: 1, label: "C2" }
-                        ]
-                    } />
+                <NavigationBar loggedIn={loggedIn} email={email} firstname={firstname} lastname={lastname} />
+                <div className="content">
+                    <Paper className="contentPaper1">
+                        <PieChart dataPoints={
+                            [
+                                { y: 7, label: "A1" },
+                                { y: 5, label: "A2" },
+                                { y: 19, label: "B1" },
+                                { y: 65, label: "B2" },
+                                { y: 3, label: "C1" },
+                                { y: 1, label: "C2" }
+                            ]
+                        } 
+                        title="Test Title"
+                        />
+                    </Paper>
                 </div>
             </div>
         );
@@ -38,4 +44,8 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(Profile); 
+export default connect(mapStateToProps)(Profile);
+
+/*
+
+*/
