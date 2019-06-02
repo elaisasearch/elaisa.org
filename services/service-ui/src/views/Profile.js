55c37a3058cc78a3c5354810f13bf276b8c67af5
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-02 13:58:25 +0200

    added progress bar

--- services/service-ui/src/views/Profile.js
+++ services/service-ui/src/views/Profile.js
@@ -6,11 +6,14 @@ import { Paper } from '@material-ui/core';
 import '../assets/css/ProfileStyle.css';
 import EnhancedTable from '../components/Profile/table/EnhancedTable';
 import axios from 'axios';
+import { CircularProgress } from "@material-ui/core";
+
 
 class Profile extends Component {
 
     state = {
-        history: []
+        history: [],
+        waiting: true
     }
 
     componentWillMount() {
@@ -20,13 +23,56 @@ class Profile extends Component {
             }
         }).then((response) => {
             this.setState({
-                history: response.data.history
+                history: response.data.history,
+                waiting: false
             })
         }).catch((error) => {
             console.log(error.message)
+            this.setState({
+                waiting: false
+            })
         })
     }
 
+
+    renderContent() {
+        // while service is fetching data, show the progress circle
+        if (this.state.waiting) {
+            return <div className="progress"><CircularProgress style={{ color: "grey" }} /></div>
+        }
+        return <div>
+            <div className="content">
+                <Paper className="contentPaper1">
+                    <PieChart dataPoints={
+                        [
+                            { y: 7, label: "A1" },
+                            { y: 5, label: "A2" },
+                            { y: 19, label: "B1" },
+                            { y: 65, label: "B2" },
+                            { y: 3, label: "C1" },
+                            { y: 1, label: "C2" }
+                        ]
+                    }
+                        title="Level"
+                    />
+                </Paper>
+                <Paper className="contentPaper2">
+                    <PieChart dataPoints={
+                        [
+                            { y: 56, label: "English" },
+                            { y: 24, label: "German" },
+                            { y: 20, label: "Spanish" }
+                        ]
+                    }
+                        title="Language"
+                    />
+                </Paper>
+            </div>
+            <div className="contentTable">
+                <EnhancedTable title="Search History" data={this.state.history} />
+            </div>
+        </div>
+    }
 
     render() {
 
@@ -36,37 +82,7 @@ class Profile extends Component {
         return (
             <div>
                 <NavigationBar loggedIn={loggedIn} email={email} firstname={firstname} lastname={lastname} />
-                <div className="content">
-                    <Paper className="contentPaper1">
-                        <PieChart dataPoints={
-                            [
-                                { y: 7, label: "A1" },
-                                { y: 5, label: "A2" },
-                                { y: 19, label: "B1" },
-                                { y: 65, label: "B2" },
-                                { y: 3, label: "C1" },
-                                { y: 1, label: "C2" }
-                            ]
-                        } 
-                        title="Level"
-                        />
-                    </Paper>
-                    <Paper className="contentPaper2">
-                        <PieChart dataPoints={
-                            [
-                                { y: 56, label: "English" },
-                                { y: 24, label: "German" },
-                                { y: 20, label: "Spanish" }
-                            ]
-                        } 
-                        title="Language"
-                        />
-                    </Paper>
-                </div>
-                <div className="contentTable">
-                    <EnhancedTable title="Search History" data={this.state.history}/>
-                </div>
-                
+                {this.renderContent()}
             </div>
         );
     }
