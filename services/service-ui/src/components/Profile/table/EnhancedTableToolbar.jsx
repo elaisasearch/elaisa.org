d881ff97c77b4f3b459f573d9984be8ba397703c
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-02 18:46:28 +0200

    style refactoring

--- services/service-ui/src/components/Profile/table/EnhancedTableToolbar.jsx
+++ services/service-ui/src/components/Profile/table/EnhancedTableToolbar.jsx
@@ -7,63 +7,60 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import PropTypes from 'prop-types';
 
-const useToolbarStyles = theme => ({
-    root: {
-      paddingLeft: "2%",//theme.spacing(2),
-      paddingRight: "1%" // theme.spacing(1),
-    },
-    highlight: {
-      color: "grey", //theme.palette.secondary.main,
-      backgroundColor: "white" //lighten(theme.palette.secondary.light, 0.85),
-    },
-    spacer: {
-      flex: '1 1 100%',
-    },
-    actions: {
-      color: "red" // theme.palette.text.secondary,
-    },
-    title: {
-      flex: '0 0 auto',
-    },
-  });
-  
-  const EnhancedTableToolbar = props => {
-    const classes = useToolbarStyles();
-    const { numSelected } = props;
-  
-    return (
-      <Toolbar
-        className={clsx(classes.root, {
-          [classes.highlight]: numSelected > 0,
-        })}
-      >
-        <div className={classes.title}>
-          {numSelected > 0 ? (
-            <Typography color="inherit" variant="subtitle1">
-              {numSelected} selected
+const useToolbarStyles = {
+  root: {
+    paddingLeft: "2%",
+    paddingRight: "1%"
+  },
+  highlight: {
+    color: "lightred",
+    backgroundColor: "lightred"
+  },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  actions: {
+    color: "red"
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+};
+
+const EnhancedTableToolbar = props => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      style={numSelected > 0 ? useToolbarStyles.highlight : useToolbarStyles.root}
+    >
+      <div style={useToolbarStyles.title}>
+        {numSelected > 0 ? (
+          <Typography color="inherit" variant="subtitle1">
+            {numSelected} selected
             </Typography>
-          ) : (
+        ) : (
             <Typography variant="h6" id="tableTitle">
               {props.title}
             </Typography>
           )}
-        </div>
-        <div className={classes.spacer} />
-        <div className={classes.actions}>
-          {numSelected > 0 ? (
-            <Tooltip title="Delete">
-              <IconButton aria-label="Delete">
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          ) : null }
-        </div>
-      </Toolbar>
-    );
-  };
-  
-  EnhancedTableToolbar.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-  };
+      </div>
+      <div style={useToolbarStyles.spacer} />
+      <div style={useToolbarStyles.actions}>
+        {numSelected > 0 ? (
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        ) : null}
+      </div>
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
 
 export default EnhancedTableToolbar;
