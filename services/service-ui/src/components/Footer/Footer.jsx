5a0f7bb42acb9dfe0394b278907c0d4a416375af
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-03 14:09:40 +0200

    typo in footer

--- services/service-ui/src/components/Footer/Footer.jsx
+++ services/service-ui/src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import FooterPerson from './FooterPerson';
 const Footer = () => (
     <footer className="footer">
         <div className="content">
-            <FooterPerson name="Jennifer Gynp" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="jennifer.gynp@hhu.de" />
-            <FooterPerson name="Alexander Teusz" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="alexander.teusz@hhu.de" />
-            <FooterPerson name="Paula Leberer" study="Information Science" faculty="Faculty of Arts and Humandities" uni="Heinrich-Heine-University Düsseldorf" mail="paula.leberer@hhu.de" />
+            <FooterPerson name="Jennifer Gynp" study="Information Science" faculty="Faculty of Arts and Humanities" uni="Heinrich-Heine-University Düsseldorf" mail="jennifer.gynp@hhu.de" />
+            <FooterPerson name="Alexander Teusz" study="Information Science" faculty="Faculty of Arts and Humanities" uni="Heinrich-Heine-University Düsseldorf" mail="alexander.teusz@hhu.de" />
+            <FooterPerson name="Paula Leberer" study="Information Science" faculty="Faculty of Arts and Humanities" uni="Heinrich-Heine-University Düsseldorf" mail="paula.leberer@hhu.de" />
         </div>
         <div className="legal">
             <Typography variant="caption" color="default">
