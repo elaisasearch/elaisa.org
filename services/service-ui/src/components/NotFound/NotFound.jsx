2421510ae55617402fb32413f175fdca54903849
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-20 19:44:44 +0200

    added 'did you mean...' button to not found view for correct spelled search value

--- services/service-ui/src/components/NotFound/NotFound.jsx
+++ services/service-ui/src/components/NotFound/NotFound.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../assets/jss/NotFoundStyle";
 import errorPic from "../../assets/img/error.png";
 import { Typography } from "@material-ui/core";
+import { Button } from '@material-ui/core';
 
 /**
  * The not found component for the Results view.
@@ -9,15 +10,40 @@ import { Typography } from "@material-ui/core";
  * @returns {JSX} Error text and image.
 */
 const NotFound = (props) => {
+
+    const { searchValue, level, language, correctSpelledQuery, onClickSpellCheck } = props;
+
+    if (correctSpelledQuery.length > 0) {
+        return (
+            <div>
+                <Button style={styles.spelling} onClick={onClickSpellCheck}>
+                    <Typography variant="caption" color="primary">
+                        Did you mean "<b>{correctSpelledQuery}</b>?".
+                    </Typography>
+                </Button>
+                <div style={styles.notFound}>
+                    <img src={errorPic} alt="Error" style={{ width: "20%" }} />
+                    <Typography variant="h6">
+                        Sorry, there are no results for "<b>{searchValue}</b>" 😔.
+                        Please try again 🧐.
+                    </Typography>
+                    <Typography variant="caption">
+                        Try to choose another level than <b>{level}</b> or another language than <b>{language.toUpperCase()}</b>
+                    </Typography>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div style={styles.root}>
+        <div style={styles.notFound}>
             <img src={errorPic} alt="Error" style={{ width: "20%" }} />
             <Typography variant="h6">
-                Sorry, there are no results for "<b>{props.searchValue}</b>" 😔.
+                Sorry, there are no results for "<b>{searchValue}</b>" 😔.
                 Please try again 🧐.
           </Typography>
             <Typography variant="caption">
-                Try to choose another level than <b>{props.level}</b> or another language than <b>{props.language.toUpperCase()}</b>
+                Try to choose another level than <b>{level}</b> or another language than <b>{language.toUpperCase()}</b>
             </Typography>
         </div>
     );
