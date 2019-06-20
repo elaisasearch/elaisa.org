17acbca8232f689249a100af72bfa84771406db3
Alexander Teusz <teusz.alexander@gmail.com>
2019-06-20 19:43:35 +0200

    added spell check file for checking the query for spelling

---
+++ services/service-api/lib/spelling.py
@@ -0,0 +1,24 @@
+"""
+Handles all spelling checks for the input text
+"""
+
+from textblob import TextBlob, Word
+
+
+def checkSpelling(text):
+    """
+    Checks if the text is spelled correctly. If not, it returns the probable right text
+    :text: String
+    """
+    word = Word(text)
+    # returns something like: [('hello', 0.7619047619047619), ('hell', 0.23809523809523808)]
+    checkResult = word.spellcheck()
+    # most probable texts probability
+    probability = checkResult[0][1]
+
+    if probability < 0.95:
+        blob = TextBlob(text)
+        # return the most probable correct word
+        return blob.correct()
+    else:
+        return text
