fa826966b8f03dcb6b4b9e05a84233da720a18d0
Alexander Teusz <teusz.alexander@gmail.com>
2019-07-15 13:20:41 +0200

    render different result navBar for mobile devices

--- services/service-ui/src/components/NavigiationBar/NavigationBar.jsx
+++ services/service-ui/src/components/NavigiationBar/NavigationBar.jsx
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button'
 import { withRouter } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
 //import styles
 import styles from '../../assets/jss/NavigationBarStyle';
@@ -16,7 +17,7 @@ import logo from '../../assets/img/logo.png';
 
 class NavigationBar extends React.Component {
 
-  constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -59,22 +60,28 @@ class NavigationBar extends React.Component {
     const { loggedIn, email, firstname, lastname } = props;
 
     if (loggedIn) {
-      if (email) return <Avatars loggedIn email={email} firstname={firstname} lastname={lastname}/>;
+      if (email) return <Avatars loggedIn email={email} firstname={firstname} lastname={lastname} />;
       else return <Avatars firstname={firstname} lastname={lastname}>AT</Avatars>;
-    } else {
-      return <Avatars/>;
+    } else {
+      return <Avatars />;
     }
   }
 
   renderSearchBar = (props) => {
     if (props.results) {
-      return <div style={styles.searchBar}>
-        <Button href="/" style={styles.elaisaButton}><img src={logo} style={styles.elaisaText} alt="Elaisa Search Engine Logo"></img></Button>
-        <SearchBarNavigationBar value={props.values[0]} onChange={e => this.setState({ value: e })} onKeyDown={this.keyPress} />
-        <DropDownMenu value={this.state.language} desc="Result Language" items={["Deutsch", "English", "Español"]} values={["de", "en", "es"]} onChange={e => this.setState({ language: e })} />
-        <DropDownMenu value={this.state.level} desc="Language Level" items={["A1", "A2", "B1", "B2", "C1", "C2"]} values={["A1", "A2", "B1", "B2", "C1", "C2"]} onChange={e => this.setState({ level: e })} />
-        <Button style={styles.searchButton} variant="contained" onClick={this.searchButtonPressed}>SEARCH</Button>
-      </div>
+      if (isMobile) {
+        return <div style={styles.searchBar}>
+          <Button href="/" style={styles.elaisaButton}><img src={logo} style={styles.elaisaText} alt="Elaisa Search Engine Logo"></img></Button>
+        </div>
+      } else {
+        return <div style={styles.searchBar}>
+          <Button href="/" style={styles.elaisaButton}><img src={logo} style={styles.elaisaText} alt="Elaisa Search Engine Logo"></img></Button>
+          <SearchBarNavigationBar value={props.values[0]} onChange={e => this.setState({ value: e })} onKeyDown={this.keyPress} />
+          <DropDownMenu value={this.state.language} desc="Result Language" items={["Deutsch", "English", "Español"]} values={["de", "en", "es"]} onChange={e => this.setState({ language: e })} />
+          <DropDownMenu value={this.state.level} desc="Language Level" items={["A1", "A2", "B1", "B2", "C1", "C2"]} values={["A1", "A2", "B1", "B2", "C1", "C2"]} onChange={e => this.setState({ level: e })} />
+          <Button style={styles.searchButton} variant="contained" onClick={this.searchButtonPressed}>SEARCH</Button>
+        </div>
+      }
     }
     return <div style={styles.defaultContent}>
       <Clock />
