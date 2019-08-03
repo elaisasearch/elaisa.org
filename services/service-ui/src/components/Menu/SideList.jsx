a5e719add7bac5a160f0a3cf16576dee28e82b74
Alexander Teusz <teusz.alexander@gmail.com>
2019-08-03 15:25:44 +0200

    readded the home menu item, since you couldn't visit the home screen after visiting your profile

--- services/service-ui/src/components/Menu/SideList.jsx
+++ services/service-ui/src/components/Menu/SideList.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { List, Divider, ListItem, ListItemIcon, ListItemText, Collapse, ListSubheader } from '@material-ui/core/';
-import { Share, ExpandLess, ExpandMore } from '@material-ui/icons/';
+import { Share, ExpandLess, ExpandMore, Home } from '@material-ui/icons/';
 import styles from '../../assets/jss/MenuStyle';
 import MenuHelper from './MenuHelper';
+import { Link } from 'react-router-dom'
 
 // import language flags for menu
 import german from '../../assets/img/menu_flags/german.jpg';
 import english from '../../assets/img/menu_flags/english.jpg';
 import spanish from '../../assets/img/menu_flags/spanish.jpg';
+
+/**
+ * Link Item for Menu to change view.
+ * @param {object} props the given properties.
+ * @returns {JSX} the button with 'a' href component.
+*/
+const ListItemLink = (props) => {
+    return <ListItem button component={Link} {...props} />;
+}
 
 class SideList extends Component {
 
@@ -21,7 +31,7 @@ class SideList extends Component {
     handleClick = () => {
         this.setState(state => ({ open: !state.open }));
     };
-    
+
     /**
      * Renders the menu list with all items.
      * @returns {JSX} the entire menu list with all items.
@@ -31,9 +41,16 @@ class SideList extends Component {
             <div style={styles.list}>
                 <List
                     component="nav"
-                    style={{maxWidth: "25vh"}}
+                    style={{ maxWidth: "25vh" }}
                     subheader={<ListSubheader component="nav">Menu</ListSubheader>}
                 >
+                    {/* Home */}
+                    <ListItemLink to="/" >
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText inset primary="Home" />
+                    </ListItemLink>
                     {/* Level of Speaking Tests MENU */}
                     <ListItem button onClick={this.handleClick}>
                         <ListItemIcon>
@@ -49,7 +66,7 @@ class SideList extends Component {
                             <MenuHelper href="https://sprachtest.de/einstufungstest-spanisch" flag={spanish} style={styles.nested} text="Español" />
                             <Divider />
                         </List>
-                    </Collapse>        
+                    </Collapse>
                 </List>
             </div>
         );
