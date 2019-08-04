ca8153487c1d66e39b66a7b00954225266de2049
Alexander Teusz <teusz.alexander@gmail.com>
2019-08-04 22:02:45 +0200

    new path to globals file in service-crawler container categorizer

--- services/service-crawler/webcrawler/spiders/categorize.py
+++ services/service-crawler/webcrawler/spiders/categorize.py
@@ -9,7 +9,7 @@ Load the global configurations for database connection and collections.
     - source: https://stackoverflow.com/questions/7165749/open-file-in-a-relative-location-in-python
 """
 scriptDir = os.path.dirname(__file__) 
-relPath = '../../../../bin/globals.json'
+relPath = 'globals.json'
 with open(os.path.join(scriptDir, relPath)) as f: 
     GLOBALS = json.load(f)
 
