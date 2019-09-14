da684c04fc4783f7b910efa04ec95c64f1918e60
Alexander Teusz <teusz.alexander@gmail.com>
2019-09-14 12:29:09 +0200

    added method to calculate term frequency (nlp.py)

--- services/service-api/lib/nlp.py
+++ services/service-api/lib/nlp.py
@@ -1,4 +1,5 @@
 import textacy
+from collections import defaultdict
 
 def extractNamedEntities(query, language):
     """
@@ -10,4 +11,32 @@ def extractNamedEntities(query, language):
     entities = list(textacy.extract.named_entities(doc))
 
     return [str(ent) for ent in entities]
+
+def calculateTermfrequency(query, ids, texts):
+    """
+    Calcualtes the TF part of the TF*IDF formular to show documents at the top of the result list
+    in which the search query occurs the most.
+    Example: 
+        - Before -> ids: ['5d7bcb60f85903a2af72c4a3', '5d7bcb6bf85903a2af72c4a6']
+        - After  -> {'5d7bcb6bf85903a2af72c4a6': {'wordFrequency': 5, 'textWordCount': 1440, 'tf': 0.003472222222222222}, 
+                    ''5d7bcb60f85903a2af72c4a3': {'wordFrequency': 1, 'textWordCount': 1494, 'tf': 0.0006693440428380187}}
+    source: https://stackoverflow.com/questions/722697/best-way-to-turn-word-list-into-frequency-dict
+    tfIdf: https://www.onely.com/blog/what-is-tf-idf/
+
+    :query: String
+    :ids: List
+    :texts: Dictionary
+
+    :return: Dictionary
+    """
+    return  {
+                id: {
+                        'wordFrequency': ids.count(id), 
+                        'textWordCount': len(texts[id].split()),
+                        'tf': ids.count(id) / len(texts[id].split())
+                } 
+                for id in set(ids)
+            }
+   
+    
 
