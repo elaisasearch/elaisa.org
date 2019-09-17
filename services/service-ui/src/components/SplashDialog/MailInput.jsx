621f22cd03dfa27ec452f02515d40512e2b15782
Alexander Teusz <teusz.alexander@gmail.com>
2019-09-17 16:03:10 +0200

    handle api responses and show response message in the dialog

--- services/service-ui/src/components/SplashDialog/MailInput.jsx
+++ services/service-ui/src/components/SplashDialog/MailInput.jsx
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Translate } from 'react-localize-redux';
@@ -13,6 +14,8 @@ import axios from 'axios';
 const MailInput = (props) => {
 
     const [email, setEmail] = useState();
+    const [showResponseMessage, setShowResponseMessage] = useState(false);
+    const [responseMessageVariant, setResponseMessageVariant] = useState();
 
     const { open, handleClose } = props;
 
@@ -23,12 +26,39 @@ const MailInput = (props) => {
 
         axios.post(`http://api.elaisa.org/forgotpassword?email=${email}`, {}
         ).then((response) => {
-            handleClose()
+            switch (response.data) {
+                case 'Success':
+                    setShowResponseMessage(true)
+                    setResponseMessageVariant('success')
+                    setTimeout(() => {
+                        handleClose();
+                    }, 2000)
+                    break;
+                case 'Mail not found':
+                    setShowResponseMessage(true)
+                    setResponseMessageVariant('mail not found')
+                    break;
+                default:
+                    setShowResponseMessage(true)
+                    setResponseMessageVariant('error')
+                    return;
+            }
         }).catch((error) => {
             console.log(error)
         })
     }
 
+    const renderResponseMessage = () => {
+        if (showResponseMessage) {
+            if (responseMessageVariant === 'success') {
+                return <Typography variant='caption' style={{color: 'green'}} >Successfully changed the password.</Typography>
+            } else if (responseMessageVariant === 'mail not found') {
+                return <Typography variant='caption' style={{color: 'red'}}>The provided mail wasn't found. Please try again with another mail.</Typography>
+            } else if (responseMessageVariant === 'error') {
+                return <Typography variant='caption' style={{color: 'red'}}>Sorry but there was an error. Please try again later.</Typography>
+            }
+        }
+    }
 
     return (
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
@@ -46,6 +76,7 @@ const MailInput = (props) => {
                     fullWidth
                     onChange={e => setEmail(e.target.value)}
                 />
+                {renderResponseMessage()}
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">
