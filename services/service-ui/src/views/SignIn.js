de37313fc12077fbc5cab57070cf39a1f4a27a2d
Alexander Teusz <teusz.alexander@gmail.com>
2019-09-17 15:34:52 +0200

    refactoring; removed Link import

--- services/service-ui/src/views/SignIn.js
+++ services/service-ui/src/views/SignIn.js
@@ -3,7 +3,6 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
