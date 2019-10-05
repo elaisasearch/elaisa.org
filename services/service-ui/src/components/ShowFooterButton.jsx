b8eef04c72348f6addb2adf7526f10928acec192
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-05 16:37:33 +0200

    added ;

--- services/service-ui/src/components/ShowFooterButton.jsx
+++ services/service-ui/src/components/ShowFooterButton.jsx
@@ -10,7 +10,7 @@ const ShowFooterButton = () => {
 
     // disable the show footer button if it was clicked one time to prevent hight bug in the footer.
     // when the user wants to the see footer, he has to scroll down manually
-    const [disableShowFooterButton, setDisableShowFooterButton] = useState(false)
+    const [disableShowFooterButton, setDisableShowFooterButton] = useState(false);
 
     return <Tooltip title={<Translate id='UI__BUTTON__SHOW_FOOTER__TOOLTIP' />} aria-label='show-more-information'>
         <IconButton onClick={e => { scrollToFooter(); setDisableShowFooterButton(true) }} disabled={disableShowFooterButton} aria-label="show-footer" id="show-footer-button" size="large">
