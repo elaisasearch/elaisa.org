e26d7eb267174533e413cea1a00cb52c10963e07
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-07 10:07:05 +0200

    using material ui makestyle for showfooter button

--- services/service-ui/src/components/ShowFooterButton.jsx
+++ services/service-ui/src/components/ShowFooterButton.jsx
@@ -2,18 +2,30 @@ import React, { useState } from 'react';
 import { IconButton, Tooltip } from '@material-ui/core/';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import { Translate } from 'react-localize-redux';
+import { makeStyles } from '@material-ui/styles';
 
 import scrollToFooter from '../handlers/scrollHandler';
 
+const useStyles = makeStyles({
+    footerButton: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        bottom: '5vh'
+    }
+});
 
 const ShowFooterButton = () => {
+    const classes = useStyles();
 
     // disable the show footer button if it was clicked one time to prevent hight bug in the footer.
     // when the user wants to the see footer, he has to scroll down manually
     const [disableShowFooterButton, setDisableShowFooterButton] = useState(false);
 
     return <Tooltip title={<Translate id='UI__BUTTON__SHOW_FOOTER__TOOLTIP' />} aria-label='show-more-information'>
-        <IconButton onClick={e => { scrollToFooter(); setDisableShowFooterButton(true) }} disabled={disableShowFooterButton} aria-label="show-footer" id="show-footer-button" size="large">
+        <IconButton className={classes.footerButton} onClick={e => { scrollToFooter(); setDisableShowFooterButton(true) }} disabled={disableShowFooterButton} aria-label="show-footer" size="large">
             <ArrowDownwardIcon fontSize="large" />
         </IconButton>
     </Tooltip>
