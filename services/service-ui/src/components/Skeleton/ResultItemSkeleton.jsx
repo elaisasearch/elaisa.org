4527a1b738317e431a0a3195e32d50d03208e7ee
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-08 14:09:59 +0200

    added circle to skeleton for computer

--- services/service-ui/src/components/Skeleton/ResultItemSkeleton.jsx
+++ services/service-ui/src/components/Skeleton/ResultItemSkeleton.jsx
@@ -8,12 +8,24 @@ const ResultItemSkeleton = () => (
         <div>
             < Skeleton variant='text' width='89vw' height={20} />
             <Skeleton variant='rect' width='90vw' height={118} />
+            <br />
+            <br />
         </div >
         :
-        <div>
-            < Skeleton variant='text' width='49vw' height={20} />
-            <Skeleton variant='rect' width='50vw' height={70} />
-        </div >
+        <div style={{
+            display: 'flex',
+            alignItems: 'center'
+        }}>
+            <Skeleton variant='circle' width={40} height={40} style={{
+                marginRight: '2%'
+            }}/>
+            <div>
+                < Skeleton variant='text' width='49vw' height={20} />
+                <Skeleton variant='rect' width='50vw' height={70} />
+                <br />
+                <br />
+            </div >
+        </div>
 
 )
 
