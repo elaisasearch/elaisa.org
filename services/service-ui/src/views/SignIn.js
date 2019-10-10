6e1d9dcabf5209f57d5a64ba854669cd6e5d916d
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-10 15:18:26 +0200

    translated sign in to func component

--- services/service-ui/src/views/SignIn.js
+++ services/service-ui/src/views/SignIn.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
+import { Typography, Fab, Grid, Paper } from '@material-ui/core/';
 import UpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { withRouter } from "react-router-dom";
 import { SnackbarProvider, withSnackbar } from 'notistack';
@@ -14,158 +11,162 @@ import { connect } from 'react-redux';
 import { Translate } from "react-localize-redux";
 import HeaderTags from '../components/HeaderTags';
 import MailInput from '../components/Dialogs/MailInput';
-
+import { isMobile } from 'react-device-detect';
+import { makeStyles } from '@material-ui/styles';
 
 import axios from 'axios';
 
 import '../assets/css/SignInStyle.css'
 import logo from '../assets/img/logo.png';
 
+const useStyles = makeStyles({
+    signinRoot: {
+        height: '100vh'
+    }
+});
+
 /**
  * SignIn view class.
  * @param {object} props the given properties.
  * @returns {JSX} signIn view jsx components.
 */
-class SignIn extends Component {
+const SignIn = (props) => {
 
-    state = {
-        email: "",
-        password: "",
-        changePasswordDialogOpen: false
-    }
+    const [password, setPassword] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [changePasswordDialogOpen, setChangePasswordDialogOpen] = React.useState(false);
 
-    handleCloseChangePasswordDialog = () => {
-        this.setState({
-            changePasswordDialogOpen: false
-        });
+    const handleCloseChangePasswordDialog = () => {
+        setChangePasswordDialogOpen(false)
     }
 
     /**
      * Checks the user's login data with API post request and stores to redux.
      * @param {event} event the clicked key event
     */
-    keyPress = (e) => {
+    const keyPress = (e) => {
         // get the input when user cliks enter (13)
         if (e.keyCode === 13) {
-            this.handleSignIn()
+            handleSignIn()
         }
     }
 
     /**
      * Checks the user's login data with API post request and stores to redux.
     */
-    handleSignIn = () => {
+    const handleSignIn = () => {
         let variant = "";
 
-        axios.post(`https://api.elaisa.org/signin?email=${this.state.email}&password=${this.state.password}`)
+        console.log('pass: ', password)
+        console.log('email: ', email)
+
+        axios.post(`https://api.elaisa.org/signin?email=${email}&password=${password}`)
             .then((response) => {
                 if (response.data.response === "Success") {
-                    
+
                     const { email, firstname, lastname } = response.data.user;
-                
+
                     // redux action
-                    this.props.onSignIn(email, firstname, lastname);
+                    props.onSignIn(email, firstname, lastname);
 
                     // navigate to App.js
-                    this.props.history.push({
+                    props.history.push({
                         pathname: "/"
                     });
 
                     variant = "success";
-                    this.props.enqueueSnackbar('Successfully logged in user', { variant });
+                    props.enqueueSnackbar('Successfully logged in user', { variant });
                 } else {
                     variant = "error"
-                    this.props.enqueueSnackbar('Password or Username incorrect', { variant });
+                    props.enqueueSnackbar('Password or Username incorrect', { variant });
                 }
             }).catch((error) => {
                 variant = "error"
-                this.props.enqueueSnackbar(error.message, { variant });
+                props.enqueueSnackbar(error.message, { variant });
             });
     }
 
-    render() {
-        return (
-            <div className="signIn">
-                <MailInput open={this.state.changePasswordDialogOpen} handleClose={this.handleCloseChangePasswordDialog}/>
-                <HeaderTags 
-                 title="Elaisa Search Engine - Sign In"
-                 desc="Sign in to the Elaisa Search Engine to store your search history and comprehend your state of knowledge in your profile."
-                 keywords="Sign In, Search History, Profile"
-                />
-                <div className="image" />
-                <Paper className="paper">
-                    <img className="signInLogo" src={logo} alt="Elaisa Search Engine Logo"></img>
-                    <Typography component="h1" variant="h5">
-                        <Translate id='UI__SIGN_IN_PAGE__TITLE' />
-                    </Typography>
-                    <form className="form" noValidate>
-                        <TextField
-                            onChange={e => this.setState({ email: e.target.value })}
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label={<Translate id='UI__SIGN_IN_PAGE__EMAIL_FIELD' />}
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            onKeyDown={this.keyPress}
-                            onChange={e => this.setState({ password: e.target.value })}
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label={<Translate id='UI__SIGN_IN_PAGE__PASSWORD_FIELD' />}
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="secondary" />}
-                            label={<Translate id='UI__SIGN_IN_PAGE__REMEMBER_ME_BUTTON' />}
-                        />
-                        <Button
-                            // type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="secondary"
-                            className="submit"
-                            onClick={this.handleSignIn.bind(this)}
-                            disabled={this.state.email.length === 0 || this.state.password.length === 0}
-                        >
-                            <Translate id='UI__SIGN_IN_PAGE__SIGN_IN_BUTTON' />
-                        </Button>
-                        <Grid container >
-                            <Grid item xs>
-                                <Button 
-                                    style={{fontSize: '70%'}}
-                                    onClick={e => this.setState({changePasswordDialogOpen: true})}
-                                >
-                                    <Translate id='UI__SIGN_IN_PAGE__FORGOT_PASSWORD_BUTTON' />
-                                </Button>
-                            </Grid>
-                            <Grid item>
-                                <Button style={{fontSize: '70%'}} id="link" href="/signup" variant="text">
-                                    <Translate id='UI__SIGN_IN_PAGE__SIGN_UP_BUTTON' />
-                                </Button>
-                            </Grid>
+    return (
+        <Grid container className="signIn">
+            <MailInput open={changePasswordDialogOpen} handleClose={handleCloseChangePasswordDialog} />
+            <HeaderTags
+                title="Elaisa Search Engine - Sign In"
+                desc="Sign in to the Elaisa Search Engine to store your search history and comprehend your state of knowledge in your profile."
+                keywords="Sign In, Search History, Profile"
+            />
+            {!isMobile ? <div className="image" /> : null}
+            <Paper className="paper">
+                <img className="signInLogo" src={logo} alt="Elaisa Search Engine Logo"></img>
+                <Typography component="h1" variant="h5">
+                    <Translate id='UI__SIGN_IN_PAGE__TITLE' />
+                </Typography>
+                <form className="form" noValidate>
+                    <TextField
+                        onChange={e => setEmail(e.target.value)}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label={<Translate id='UI__SIGN_IN_PAGE__EMAIL_FIELD' />}
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        onKeyDown={keyPress}
+                        onChange={e => setPassword(e.target.value)}
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label={<Translate id='UI__SIGN_IN_PAGE__PASSWORD_FIELD' />}
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="secondary" />}
+                        label={<Translate id='UI__SIGN_IN_PAGE__REMEMBER_ME_BUTTON' />}
+                    />
+                    <Button
+                        // type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className="submit"
+                        onClick={handleSignIn}
+                        disabled={email.length === 0 || password.length === 0}
+                    >
+                        <Translate id='UI__SIGN_IN_PAGE__SIGN_IN_BUTTON' />
+                    </Button>
+                    <Grid container >
+                        <Grid item xs>
+                            <Button
+                                style={{ fontSize: '70%' }}
+                                onClick={e => setChangePasswordDialogOpen(true)}
+                            >
+                                <Translate id='UI__SIGN_IN_PAGE__FORGOT_PASSWORD_BUTTON' />
+                            </Button>
                         </Grid>
-                    </form>
-                    <div className="fab">
-                        <Fab href="/" color="secondary">
-                            <UpIcon />
-                        </Fab>
-                    </div>
+                        <Grid item>
+                            <Button style={{ fontSize: '70%' }} id="link" href="/signup" variant="text">
+                                <Translate id='UI__SIGN_IN_PAGE__SIGN_UP_BUTTON' />
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+                <div className="fab">
+                    <Fab href="/" color="secondary">
+                        <UpIcon />
+                    </Fab>
+                </div>
 
-                </Paper>
-            </div>
-        );
-    }
-};
+            </Paper>
+        </Grid>
+    );
+}
 
 /**
  * Maps redux signIn action to props.
