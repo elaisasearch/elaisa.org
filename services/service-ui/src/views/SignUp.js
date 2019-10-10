1d862986f1d7ab7291595a59f910604a159fd401
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-10 16:12:44 +0200

    refactored design with material ui styles

--- services/service-ui/src/views/SignUp.js
+++ services/service-ui/src/views/SignUp.js
@@ -1,67 +1,102 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
+import React from 'react';
+import { Fab, Grid, Typography, Paper, Link, Checkbox, FormControlLabel, TextField, Button } from '@material-ui/core/';
 import UpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { withRouter } from "react-router-dom";
 import { SnackbarProvider, withSnackbar } from 'notistack';
 import { Translate } from "react-localize-redux";
 import HeaderTags from '../components/HeaderTags';
-
+import { isMobile } from 'react-device-detect';
+import { makeStyles } from '@material-ui/styles';
 
 import '../assets/css/SignUpStyle.css'
 import logo from '../assets/img/logo.png';
 
 import axios from 'axios';
 
+const useStyles = makeStyles({
+    signupRoot: {
+        height: '100vh'
+    },
+    backFab: {
+        width: '90%',
+        margin: '5%'
+    },
+    formPaper: {
+        display: 'flex',
+        flex: isMobile ? 1 : 0.7,
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    sideImage: {
+        backgroundImage: 'url(https://source.unsplash.com/random);',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    },
+    signUpLogo: {
+        width: '25%',
+        marginTop: '10%',
+        marginBottom: '5%'
+    },
+    form: {
+        width: '90%',
+        margin: '5%'
+    },
+    divNames: {
+        display: isMobile ? null : 'flex',
+        justifyContent: 'flex-end'
+    },
+    signUpSubtitle: {
+        width: isMobile ? '80%' : null,
+        textAlign: isMobile ? 'center' : null
+    },
+    agreeTermsButton: {
+        marginTop: '5%',
+        marginBottom: isMobile ? '5%' : null
+    }
+});
+
 /**
  * SignUp view class.
  * @param {object} props the given properties.
  * @returns {JSX} signUp view jsx components.
 */
-class SignUp extends Component {
+const SignUp = (props) => {
 
-    state = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        agreesTermsOfService: false
-    }
+    const [firstName, setFirstName] = React.useState('');
+    const [lastName, setLastName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [agreesTermsOfService, setAgreesTermsOfService] = React.useState(false);
+
+    const classes = useStyles();
 
     /**
      * checks user's sign up data with API request and changes url path.
     */
-    handleSubmit() {
+    const handleSubmit = () => {
         let variant = "";
 
-        let { firstName, lastName, email, password } = this.state;
-
         axios.post(`https://api.elaisa.org/signup?firstname=${firstName}&lastname=${lastName}&email=${email}&password=${password}`)
-        .then((response) => {
-            if (response.data === "Success") {
+            .then((response) => {
+                if (response.data === "Success") {
 
-                setTimeout(() => {
-                    this.props.history.push({
-                        pathname: "/signin",
-                      });
-                }, 2000)
+                    setTimeout(() => {
+                        props.history.push({
+                            pathname: "/signin",
+                        });
+                    }, 2000)
 
-                variant = "success";
-                this.props.enqueueSnackbar('Successfully created the user', { variant });
-            } else if (response.data === "Error") {
+                    variant = "success";
+                    props.enqueueSnackbar('Successfully created the user', { variant });
+                } else if (response.data === "Error") {
+                    variant = "error"
+                    props.enqueueSnackbar('The user already exists', { variant });
+                }
+            }).catch((error) => {
                 variant = "error"
-                this.props.enqueueSnackbar('The user already exists', { variant });
-            }
-        }).catch((error) => {
-            variant = "error"
-            this.props.enqueueSnackbar(error.message, { variant });
-        });
+                props.enqueueSnackbar(error.message, { variant });
+            });
 
     }
 
@@ -69,108 +104,103 @@ class SignUp extends Component {
      * Renders JSX content.
      * @returns {JSX} SignUp.js.
     */
-    render() {
-        return (
-            <div className="signIn">
-                <HeaderTags 
-                 title="Elaisa Search Engine - Sign Up"
-                 desc="Sign Up to the Elaisa Search Engine to store your search history and comprehend your state of knowledge in your profile."
-                 keywords="Sign Up, Search History, Profile"
-                />
-                <div className="image" />
-                <Paper className="paper">
-                    <img className="signUpLogo" src={logo} alt="Elaisa Search Engine Logo"></img>
-                    <Typography component="h1" variant="h5">
-                        <Translate id='UI__SIGN_UP_PAGE__TITLE' />
-                    </Typography>
-                    <Typography variant="body1">
-                        <Translate id='UI__SIGN_UP_PAGE__SUBTITLE' />
-                    </Typography>
-                    <form className="form" noValidate>
+    return (
+        <Grid container className={classes.signupRoot}>
+            <HeaderTags
+                title="Elaisa Search Engine - Sign Up"
+                desc="Sign Up to the Elaisa Search Engine to store your search history and comprehend your state of knowledge in your profile."
+                keywords="Sign Up, Search History, Profile"
+            />
+            {!isMobile ? <Grid item xs className={classes.sideImage} /> : null}
+            <Paper className={classes.formPaper}>
+                <img className={classes.signUpLogo} src={logo} alt="Elaisa Search Engine Logo"></img>
+                <Typography component="h1" variant="h5">
+                    <Translate id='UI__SIGN_UP_PAGE__TITLE' />
+                </Typography>
+                <Typography variant="body1" className={classes.signUpSubtitle}>
+                    <Translate id='UI__SIGN_UP_PAGE__SUBTITLE' />
+                </Typography>
+                <form className={classes.form} noValidate>
 
-                        <div className="divNames">
-                            <TextField
-                                onChange={e => this.setState({ firstName: e.target.value })}
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label={<Translate id='UI__SIGN_UP_PAGE__FIRSTNAME_FIELD' />}
-                            />
-                            <TextField
-                                onChange={e => this.setState({ lastName: e.target.value })}
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label={<Translate id='UI__SIGN_UP_PAGE__LASTNAME_FIELD' />}
-                                name="lastName"
-                                autoComplete="lname"
-                            />
-                        </div>
-                        <div style={{ marginTop: "5%" }}>
-                            <TextField
-                                onChange={e => this.setState({ email: e.target.value })}
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label={<Translate id='UI__SIGN_UP_PAGE__EMAIL_FIELD' />}
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </div>
-                        <div style={{ marginTop: "5%" }}>
-                            <TextField
-                                onChange={e => this.setState({ password: e.target.value })}
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label={<Translate id='UI__SIGN_UP_PAGE__PASSWORD_FIELD' />}
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </div>
-                        <div style={{ marginTop: "5%" }}>
-                            <FormControlLabel
-                                control={<Checkbox value="agreeTermsOfService" color="secondary" />}
-                                label={<Translate id='UI__SIGN_UP_PAGE__AGREE_TERMS_BUTTON' />}
-                                onClick={e => this.setState({ agreesTermsOfService: e.target.checked })}
-                            />
-                        </div>
-                        <Button
-                            //type="submit"
+                    <div className={classes.divNames}>
+                        <TextField
+                            onChange={e => setFirstName(e.target.value)}
+                            autoComplete="fname"
+                            name="firstName"
+                            variant="outlined"
+                            required
                             fullWidth
-                            variant="contained"
-                            color="secondary"
-                            className="submit"
-                            onClick={this.handleSubmit.bind(this)}
-                            disabled={this.state.firstName.length === 0 || this.state.lastName.length === 0 || this.state.email.length === 0 || this.state.password.length === 0 || !this.state.agreesTermsOfService} 
-                        >
-                            <Translate id='UI__SIGN_UP_PAGE__SIGN_UP_BUTTON' />
-                        </Button>
-                        <div className="backLink">
-                            <Link href="/signin" variant="body2" id="signInLink">
-                                <Translate id='UI__SIGN_UP_PAGE__SIGN_IN_BUTTON' />
-                            </Link>
-                        </div>
-                    </form>
-                    <div className="fab">
-                        <Fab href="/signin" color="secondary">
-                            <UpIcon />
-                        </Fab>
+                            id="firstName"
+                            label={<Translate id='UI__SIGN_UP_PAGE__FIRSTNAME_FIELD' />}
+                        />
+                        <TextField
+                            onChange={e => setLastName(e.target.value)}
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="lastName"
+                            label={<Translate id='UI__SIGN_UP_PAGE__LASTNAME_FIELD' />}
+                            name="lastName"
+                            autoComplete="lastName"
+                            style={{ marginTop: isMobile ? '5%' : null }}
+                        />
                     </div>
-
-                </Paper>
-            </div>
-        );
-    }
-
-};
+                    <div style={{ marginTop: "5%" }}>
+                        <TextField
+                            onChange={e => setEmail(e.target.value)}
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label={<Translate id='UI__SIGN_UP_PAGE__EMAIL_FIELD' />}
+                            name="email"
+                            autoComplete="email"
+                        />
+                    </div>
+                    <div style={{ marginTop: "5%" }}>
+                        <TextField
+                            onChange={e => setPassword(e.target.value)}
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="password"
+                            label={<Translate id='UI__SIGN_UP_PAGE__PASSWORD_FIELD' />}
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                    </div>
+                    <div className={classes.agreeTermsButton}>
+                        <FormControlLabel
+                            control={<Checkbox value="agreeTermsOfService" color="secondary" />}
+                            label={<Translate id='UI__SIGN_UP_PAGE__AGREE_TERMS_BUTTON' />}
+                            onClick={e => setAgreesTermsOfService(e.target.checked)}
+                        />
+                    </div>
+                    <Button
+                        //type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className="submit"
+                        onClick={handleSubmit}
+                        disabled={firstName.length === 0 || lastName.length === 0 || email.length === 0 || password.length === 0 || !agreesTermsOfService}
+                    >
+                        <Translate id='UI__SIGN_UP_PAGE__SIGN_UP_BUTTON' />
+                    </Button>
+                    <Button style={{ fontSize: '70%' }} href="/signin" variant='text'>
+                        <Translate id='UI__SIGN_UP_PAGE__SIGN_IN_BUTTON' />
+                    </Button>
+                </form>
+                <Grid container xs alignItems='flex-end' direction='column-reverse' className={classes.backFab}>
+                    <Fab href="/signin" color="secondary">
+                        <UpIcon />
+                    </Fab>
+                </Grid>
+            </Paper>
+        </Grid>
+    );
+}
 
 const SignUpSnackBar = withSnackbar(withRouter(SignUp));
 
@@ -179,11 +209,11 @@ const SignUpSnackBar = withSnackbar(withRouter(SignUp));
  * @returns {JSX} the SignUp View Snack Bar Integration.
 */
 const SignUpIntegrationNotistack = () => {
-  return (
-    <SnackbarProvider maxSnack={3}>
-      <SignUpSnackBar />
-    </SnackbarProvider>
-  );
+    return (
+        <SnackbarProvider maxSnack={3}>
+            <SignUpSnackBar />
+        </SnackbarProvider>
+    );
 }
 
 export default SignUpIntegrationNotistack;
