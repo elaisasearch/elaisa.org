711024dc08f6d9a4a71ee78f3d7d0dbb4e1327c2
Alexander Teusz <teusz.alexander@gmail.com>
2019-10-12 13:46:46 +0200

    spacing 1 for dropdown menu

--- services/service-ui/src/components/DropDownMenu.jsx
+++ services/service-ui/src/components/DropDownMenu.jsx
@@ -10,7 +10,7 @@ import Select from "@material-ui/core/Select";
 
 const styles = theme => ({
   formControl: {
-    margin: theme.spacing(2),
+    margin: theme.spacing(1),
     minWidth: 160
   }
 });
