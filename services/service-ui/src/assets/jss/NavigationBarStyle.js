26c754fcab4bb70d5b88618993ed99f5ca1d6fb2
Alexander Teusz <teusz.alexander@gmail.com>
2019-12-08 17:36:47 +0100

    format

--- services/service-ui/src/assets/jss/NavigationBarStyle.js
+++ services/service-ui/src/assets/jss/NavigationBarStyle.js
@@ -1,16 +1,16 @@
 const styles = {
-    elaisaText: {
-      width: "90%",
-      maxWidth: "100px",
-      marginLeft: "4.5vh",
-      marginRight: "1%"
-    },
-    elaisaButton: {
-      backgroundColor: 'transparent',
-      "&:hover": {
-        backgroundColor: "transparent"
+  elaisaText: {
+    width: "90%",
+    maxWidth: "100px",
+    marginLeft: "4.5vh",
+    marginRight: "1%"
+  },
+  elaisaButton: {
+    backgroundColor: 'transparent',
+    "&:hover": {
+      backgroundColor: "transparent"
     }
-    }
-  };
+  }
+};
 
 export default styles;
