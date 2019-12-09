e99bbccac024ca8458290fcdcd39d8b4d06acc66
Alexander Teusz <teusz.alexander@gmail.com>
2019-12-09 09:51:05 +0100

    disable login button while loading and show progress to the user

--- services/service-ui/src/views/SignIn.js
+++ services/service-ui/src/views/SignIn.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import { Typography, Fab, Grid, Paper } from '@material-ui/core/';
+import { Typography, Fab, Grid, Paper, CircularProgress } from '@material-ui/core/';
 import UpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { withRouter } from "react-router-dom";
 import { SnackbarProvider, withSnackbar } from 'notistack';
@@ -15,12 +15,13 @@ import { isMobile } from 'react-device-detect';
 import { makeStyles } from '@material-ui/styles';
 import globals from '../globals.json';
 import { Link } from "react-router-dom";
+import { green } from '@material-ui/core/colors';
 
 import axios from 'axios';
 
 import logo from '../assets/img/logo.png';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles( theme => ({
     signinRoot: {
         height: '100vh'
     },
@@ -56,9 +57,21 @@ const useStyles = makeStyles({
         margin: '5%',
         flex: 1,
         display: isMobile ? 'none' : 'inherit'
-    }
+    },
+    buttonWrapper: {
+        position: 'relative',
+        margin: theme.spacing(1),
+    },
+    buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
 
-});
+}));
 
 /**
  * SignIn view class.
@@ -70,6 +83,8 @@ const SignIn = (props) => {
     const [password, setPassword] = React.useState('');
     const [email, setEmail] = React.useState('');
     const [changePasswordDialogOpen, setChangePasswordDialogOpen] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
 
     const classes = useStyles();
 
@@ -93,6 +108,7 @@ const SignIn = (props) => {
     */
     const handleSignIn = () => {
         let variant = "";
+        setLoading(true);
 
         axios.post(`https://api.elaisa.org/signin?email=${email}&password=${password}&key=${globals['api']['x-api-key']}`)
             .then((response) => {
@@ -101,11 +117,15 @@ const SignIn = (props) => {
                 const { message, user } = result;
                 
                 if (message === 'success') {
-
+                    
                     const { email, firstname, lastname } = user;
 
                     // redux action
                     props.onSignIn(email, firstname, lastname);
+
+                    //loading circle in button
+                    setLoading(false);
+                    setSuccess(true);
 
                     // save login state to localStorage
                     localStorage.setItem('user', JSON.stringify({
@@ -124,6 +144,9 @@ const SignIn = (props) => {
                     props.enqueueSnackbar(`Successfully logged in user ${email}`, { variant });
                 }
             }).catch((error) => {
+                setLoading(false);
+                setSuccess(false);
+
                 variant = 'error';
                 props.enqueueSnackbar('Password or Username incorrect', { variant });
             });
@@ -173,17 +196,20 @@ const SignIn = (props) => {
                         control={<Checkbox value="remember" color="secondary" />}
                         label={<Translate id='UI__SIGN_IN_PAGE__REMEMBER_ME_BUTTON' />}
                     />
-                    <Button
-                        // type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="secondary"
-                        className="submit"
-                        onClick={handleSignIn}
-                        disabled={email.length === 0 || password.length === 0}
-                    >
-                        <Translate id='UI__SIGN_IN_PAGE__SIGN_IN_BUTTON' />
-                    </Button>
+                    <div className={classes.buttonWrapper}>
+                        <Button
+                            // type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            className="submit"
+                            onClick={handleSignIn}
+                            disabled={email.length === 0 || password.length === 0 || loading}
+                        >
+                            <Translate id='UI__SIGN_IN_PAGE__SIGN_IN_BUTTON' />
+                        </Button>
+                        {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                    </div>
                     <Grid container >
                         <Grid item xs='auto'>
                             <Button
@@ -194,7 +220,7 @@ const SignIn = (props) => {
                             </Button>
                         </Grid>
                         <Grid item xs='auto'>
-                            <Button style={{ fontSize: '70%' }} href="/signup" variant="text">
+                            <Button style={{ fontSize: '70%' }} href="/signup" variant="text" >
                                 <Translate id='UI__SIGN_IN_PAGE__SIGN_UP_BUTTON' />
                             </Button>
                         </Grid>
