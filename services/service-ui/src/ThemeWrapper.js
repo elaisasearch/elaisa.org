250d947dd6cb72e6656e8a13d8b4b0d0b3a80eec
Alexander Teusz <teusz.alexander@gmail.com>
2019-12-09 09:08:59 +0100

    format

--- services/service-ui/src/ThemeWrapper.js
+++ services/service-ui/src/ThemeWrapper.js
@@ -5,39 +5,39 @@ import { createMuiTheme } from "@material-ui/core";
 
 const ThemeWrapper = (props) => {
 
-    const { children } = props;
+  const { children } = props;
 
-    // get current browser mode from user
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // get current browser mode from user
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-    const theme = React.useMemo(
-        () =>
-          createMuiTheme({
-            spacing: 4,
-            resultItem: {
-                link: {
-                    color: prefersDarkMode ? '#fff' : null
-                },
-                keywords: {
-                    color: prefersDarkMode ? '#b3e5fc' : '#00F'
-                }
-    
-            },
-            palette: {
-              type: prefersDarkMode ? 'dark' : 'light',
-              primary: {
-                main: !prefersDarkMode ? '#424242' : '#e0e0e0'
-            }
-            },
-          }),
-        [prefersDarkMode],
-      );
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        spacing: 4,
+        resultItem: {
+          link: {
+            color: prefersDarkMode ? '#fff' : null
+          },
+          keywords: {
+            color: prefersDarkMode ? '#b3e5fc' : '#00F'
+          }
 
-    return (
-        <ThemeProvider theme={theme}>
-           {children}
-        </ThemeProvider>
-    );
+        },
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          primary: {
+            main: !prefersDarkMode ? '#424242' : '#e0e0e0'
+          }
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
 }
 
 export default ThemeWrapper;
