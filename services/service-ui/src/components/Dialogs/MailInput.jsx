987a1a290a0acab8aec97a08f0e9416f9a1c909f
Alexander Teusz <teusz.alexander@gmail.com>
2020-02-22 13:56:11 +0100

    only allow reset password, if the mail input is valid

--- services/service-ui/src/components/Dialogs/MailInput.jsx
+++ services/service-ui/src/components/Dialogs/MailInput.jsx
@@ -7,26 +7,58 @@ import DialogContent from '@material-ui/core/DialogContent';
 import Typography from '@material-ui/core/Typography';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { CircularProgress } from '@material-ui/core';
 import { Translate } from 'react-localize-redux';
 import axios from 'axios';
 import globals from '../../globals.json';
+import { makeStyles } from '@material-ui/styles';
+import { green } from '@material-ui/core/colors';
 
+
+const useStyles = makeStyles({
+    buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
+    buttonWrapper: {
+        position: 'relative',
+    }
+});
 
 const MailInput = (props) => {
 
-    const [email, setEmail] = useState();
+    const [email, setEmail] = useState('');
     const [showResponseMessage, setShowResponseMessage] = useState(false);
     const [responseMessageVariant, setResponseMessageVariant] = useState();
+    const [loading, setLoading] = React.useState(false);
 
     const { open, handleClose } = props;
+
+    const classes = useStyles();
 
     /**
      * Send the new password mail and close the dialog
      */
     const handleSendPassword = () => {
 
+        // If the user input isn't a valid email address, show the error message.
+        if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            setShowResponseMessage(true)
+            setResponseMessageVariant('error')
+            return
+        }
+
+        setLoading(true);
+
         axios.post(`https://api.elaisa.org/forgotpassword?email=${email}&key=${globals['api']['x-api-key']}`, {}
         ).then((response) => {
+
+            setLoading(false);
+            
             switch (response.data.result.message) {
                 case 'success':
                     setShowResponseMessage(true)
@@ -84,9 +116,12 @@ const MailInput = (props) => {
                 <Button onClick={handleClose} color="primary">
                     <Translate id='UI__SIGN_IN_PAGE__DIALOG_MAIL_INPUT__CANCEL_BUTTON' />
                 </Button>
-                <Button onClick={handleSendPassword} color="primary">
-                    <Translate id='UI__SIGN_IN_PAGE__DIALOG_MAIL_INPUT__SUBMIT_BUTTON' />
-                </Button>
+                <div className={classes.buttonWrapper}>
+                    <Button onClick={handleSendPassword} color="primary" disabled={loading || email.length === 0}>
+                        <Translate id='UI__SIGN_IN_PAGE__DIALOG_MAIL_INPUT__SUBMIT_BUTTON' />
+                    </Button>
+                    {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                </div>
             </DialogActions>
         </Dialog>
     );
